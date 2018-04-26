38922ec57205bbeb8e1f27212a1f82f87ecf55ed
HaiBV <buivuhai.bk@gmail.com>
2018-04-26 17:59:17 +0700

    Init scoreboard project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from 'containers/App';
+import Scoreboard from 'containers/Scoreboard';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Scoreboard />, document.getElementById('score-board-application'));
+
 registerServiceWorker();
