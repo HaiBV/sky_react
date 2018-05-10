3b18e6fd536bc428b4cded697792392585b1b180
Bui Vu Hai <buivuhai.bk@gmail.com>
2018-05-10 23:33:24 +0700

    #30daysToCode #day4 Start with React Router Basics

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import logo from 'logo.svg';
 import './App.css';
+import Scoreboard from 'containers/Scoreboard/Scoreboard';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <div className="score-board-application">
+                        <Route path="/scoreboard" component={Scoreboard} />
+                    </div>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
