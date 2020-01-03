0a172c6233a9e1191aa64f371b6f2e5a116292cb
HaiBV <buivuhai.bk@gmail.com>
2020-01-03 20:27:05 +0700

    Prepare for deploy

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require('app-module-path').addPath('.');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var routers = require('routers');
+const routers = require('routers');
+const path = require('path');
 
 const connectDB = require('config/db');
 
-var app = express();
+const app = express();
 
 connectDB();
 
@@ -24,6 +24,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routers);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
